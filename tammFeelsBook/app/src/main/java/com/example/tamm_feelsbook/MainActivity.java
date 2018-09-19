abdd152987ca975fc9dad68deb44a162df8f43f3
PTamm <tamm@ualberta.ca>
2018-09-19 00:53:07 -0600

    Started to add code to switch between activities

--- tammFeelsBook/app/src/main/java/com/example/tamm_feelsbook/MainActivity.java
+++ tammFeelsBook/app/src/main/java/com/example/tamm_feelsbook/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.tamm_feelsbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addFeeling(View view){
+        Toast.makeText(this, "Add a Feeling", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(MainActivity.this, add_feeling.class);
+        startActivity(intent);
     }
 }
