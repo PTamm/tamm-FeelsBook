d0d0c7ff16a9c1d81ce3e7a6f58e570a450151e8
PTamm <tamm@ualberta.ca>
2018-10-04 15:44:50 -0600

    Modified the ModifyFeeling activity

--- tammFeelsBook/app/src/main/java/com/example/tamm_feelsbook/ModifyFeeling.java
+++ tammFeelsBook/app/src/main/java/com/example/tamm_feelsbook/ModifyFeeling.java
@@ -33,25 +33,17 @@ public class ModifyFeeling extends AppCompatActivity {
 
     private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     private final ArrayList<String> stringFeelingList = new ArrayList<String>();
-    private static final String FILENAME = "savedFeels.sav";
+    private static final String FILENAME = "file.sav";
     ArrayList<Feeling> feelingList;
+    ArrayAdapter<Feeling> feelsAdapter;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_feeling);
-        ListView listView = (ListView) findViewById(R.id.modifyFeelsList);
-        loadFromFile();
-        for (Feeling feel : feelingList){
-            if (feel.getComment()==null){
-                stringFeelingList.add(feel.getFeel()+"\n"+sdf.format(feel.getDate()));
-            } else {
-                stringFeelingList.add(feel.getFeel()+"\n"+feel.getComment()+"\n"+sdf.format(feel.getDate()));
-            }
-        }
-        final ArrayAdapter<String> feelsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, stringFeelingList);
-        listView.setAdapter(feelsAdapter);
 
+        listView = (ListView) findViewById(R.id.modifyFeelsList);
 
         // This came from Abram Hindle's 'Student Picker for Android: 6 ListView, ArrayAdapter and Observer Pattern' video
         // https://www.youtube.com/watch?v=7zKCuqScaRE&index=6&list=PL240uJOh_Vb4PtMZ0f7N8ACYkCLv0673O
@@ -69,7 +61,7 @@ public class ModifyFeeling extends AppCompatActivity {
                  *   User: https://stackoverflow.com/users/1706269/mark
                  *   Source: https://stackoverflow.com/questions/14480788/alertdialog-builder-setitems-list-with-another
                  *   Date: 2018-09-30
-                */
+                 */
 
                 modifyAlert.setTitle("Modifying "+feelingList.get(position)+"?");
                 modifyAlert.setCancelable(true);
@@ -120,31 +112,25 @@ public class ModifyFeeling extends AppCompatActivity {
                                     "Cancel",
                                     Toast.LENGTH_SHORT).show();
                         }
+                        saveToFile();
+                        feelsAdapter.notifyDataSetChanged();
                     }
                 });
-                saveToFile();
+                //saveToFile();
                 modifyAlert.show();
                 return false;
             }
         });
+
     }
 
     @Override
-    protected void onRestart(){
-        super.onRestart();
+    protected void onStart(){
+        super.onStart();
         setContentView(R.layout.activity_edit_feeling);
-        ListView listView = (ListView) findViewById(R.id.modifyFeelsList);
 
-        for (Feeling feel : feelingList){
-            if (feel.getComment()==null){
-                stringFeelingList.add(feel.getFeel()+"\n"+sdf.format(feel.getDate()));
-            } else {
-                stringFeelingList.add(feel.getFeel()+"\n"+feel.getComment()+"\n"+sdf.format(feel.getDate()));
-            }
-        }
-        final ArrayAdapter<String> feelsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, stringFeelingList);
+        feelsAdapter = new ArrayAdapter<Feeling>(this, android.R.layout.simple_list_item_1, feelingList);
         listView.setAdapter(feelsAdapter);
-
 
         // This came from Abram Hindle's 'Student Picker for Android: 6 ListView, ArrayAdapter and Observer Pattern' video
         // https://www.youtube.com/watch?v=7zKCuqScaRE&index=6&list=PL240uJOh_Vb4PtMZ0f7N8ACYkCLv0673O
@@ -156,24 +142,68 @@ public class ModifyFeeling extends AppCompatActivity {
                 //Abram Hindle, how to use AlertDialog, https://www.youtube.com/watch?v=7zKCuqScaRE&list=PL240uJOh_Vb4PtMZ0f7N8ACYkCLv0673O&index=6, 2018-09-29
                 final int pos = position;
                 AlertDialog.Builder modifyAlert = new AlertDialog.Builder(ModifyFeeling.this);
-                modifyAlert.setMessage("Modifying "+feelingList.get(position)+"?");
+
+                /* Idea to use setItems(): https://developer.android.com/reference/android/app/AlertDialog.Builder#setItems(int, android.content.DialogInterface.OnClickListener), 2018-09-30
+                 * How to implement:
+                 *   User: https://stackoverflow.com/users/1706269/mark
+                 *   Source: https://stackoverflow.com/questions/14480788/alertdialog-builder-setitems-list-with-another
+                 *   Date: 2018-09-30
+                 */
+
+                modifyAlert.setTitle("Modifying "+feelingList.get(position)+"?");
                 modifyAlert.setCancelable(true);
-                modifyAlert.setPositiveButton("Add comment", new DialogInterface.OnClickListener() {
+
+                final CharSequence[] choices = {"Add Comment","Change Feeling","Change Date","Delete This Feeling","Cancel"};
+
+                modifyAlert.setItems(choices, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Feeling feel = feelingList.get(pos);
-                        //getting user input, https://developer.android.com/training/basics/firstapp/starting-activity#java, 2018-09-29
-                        EditText editText = (EditText) findViewById(R.id.modifyComment);
-                        String comment = editText.getText().toString();
-                        feel.setComment(comment);
+                        if (which == 0){ //Add comment ...
+                            Feeling feel = feelingList.get(pos);
+                            //getting user input, https://developer.android.com/training/basics/firstapp/starting-activity#java, 2018-09-29
+                            EditText editText = (EditText) findViewById(R.id.modifyComment);
+                            String comment = editText.getText().toString();
+                            feel.setComment(comment);
+                            Toast.makeText(ModifyFeeling.this,
+                                    "Successfully added '"+comment+"' to "+feel.getFeel(),
+                                    Toast.LENGTH_LONG).show();
+                        } else if (which == 1){ //Change Feeling ...
+                            Feeling feel = feelingList.get(pos);
+                            //getting user input, https://developer.android.com/training/basics/firstapp/starting-activity#java, 2018-09-29
+                            EditText editText = (EditText) findViewById(R.id.modifyComment);
+                            String newFeel = editText.getText().toString();
+                            String oldFeel = feel.getFeel();
+                            feel.setFeel(newFeel);
+                            Toast.makeText(ModifyFeeling.this,
+                                    "Successfully changed "+oldFeel+" to "+newFeel,
+                                    Toast.LENGTH_LONG).show();
+                        } else if (which == 2){ //Change Date ...
+                            Feeling feel = feelingList.get(pos);
+                            //getting user input, https://developer.android.com/training/basics/firstapp/starting-activity#java, 2018-09-29
+                            EditText editText = (EditText) findViewById(R.id.modifyComment);
+                            String newDate = editText.getText().toString();
+                            //handling exceptions, lonelyTwitter code, Cody Rosevear, 2018-09-30
+                            try{
+                                feel.setDate(newDate);
+                            } catch (ParseException prsEx){
+                                Toast.makeText(ModifyFeeling.this, "Parse Exception encountered.",Toast.LENGTH_SHORT).show();
+                            }
+                            Toast.makeText(ModifyFeeling.this,
+                                    "Changing date to "+newDate,
+                                    Toast.LENGTH_LONG).show();
+                        } else if (which == 3) { //Delete this feeling ...
+                            Toast.makeText(ModifyFeeling.this, "You are deleting "+feelingList.get(pos).toString(), Toast.LENGTH_SHORT).show();
+                            Feeling feel = feelingList.get(pos);
+                        } else { //Last item clicked; cancel ...
+                            Toast.makeText(ModifyFeeling.this,
+                                    "Cancel",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        saveToFile();
+                        feelsAdapter.notifyDataSetChanged();
                     }
                 });
-                modifyAlert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        //Do nothing ...
-                    }
-                });
+                //saveToFile();
                 modifyAlert.show();
                 return false;
             }
@@ -185,6 +215,7 @@ public class ModifyFeeling extends AppCompatActivity {
         feelingList.clear();
         stringFeelingList.clear();
         saveToFile();
+        feelsAdapter.notifyDataSetChanged();
     }
 
     //Using Gson and file input/output came from lonelyTwitter, Joshua Campbell (2015-09-14), Abdul Ali Bangash, 2018-10-02
